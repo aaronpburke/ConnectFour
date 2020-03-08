3af0903aa020bc45f8139ddc8d7692ee9415afa4
Aaron Burke <aburke@scaleoutsoftware.com>
2020-03-08 12:39:12 -0700

    More game service tests

--- src/ConnectFour.UnitTests/GameServiceTests.cs
+++ src/ConnectFour.UnitTests/GameServiceTests.cs
@@ -1,8 +1,10 @@
 ﻿using ConnectFour.DataLayer.Models;
+using ConnectFour.ServiceLayer;
 using ConnectFour.ServiceLayer.GameService;
 using FluentAssertions;
 using Microsoft.Data.Sqlite;
 using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using Xunit;
 
@@ -78,6 +80,144 @@ namespace ConnectFour.UnitTests
                 details.Players.Should().BeEquivalentTo(newGameDetails.Players);
                 details.State.Should().Be(Game.GameState.IN_PROGRESS);
                 details.Winner.Should().BeNullOrEmpty();
+            }
+        }
+
+        [Fact]
+        public void DropPlayer()
+        {
+            using (var context = DbTestHelper.GetNewContext())
+            {
+                var sut = new GameService(context, new ServiceLayer.Models.GameOptions());
+                NewGameDetails newGameDetails = new NewGameDetails()
+                {
+                    Columns = 4,
+                    Rows = 4,
+                    Players = new List<string>() { "Player1", "Player2" }
+                };
+                var createdGameId = sut.CreateNewGame(newGameDetails);
+                createdGameId.Should().NotBeNullOrEmpty();
+
+                // Verify initial players list
+                {
+                    var details = sut.GetGameDetails(createdGameId);
+                    details.Should().NotBeNull();
+                    details.Players.Should().BeEquivalentTo(newGameDetails.Players);
+                }
+
+                // Make sure an invalid player cannot be removed
+                Action act = () => sut.RemovePlayer(createdGameId, "Invalid Player");
+                act.Should().Throw<PlayerNotFoundException>();
+
+                {
+                    var details = sut.GetGameDetails(createdGameId);
+                    details.Should().NotBeNull();
+                    details.Players.Should().BeEquivalentTo(newGameDetails.Players);
+                }
+
+                // Remove player 2
+                {
+                    sut.RemovePlayer(createdGameId, "Player2");
+
+                    // Verify
+                    var details = sut.GetGameDetails(createdGameId);
+                    details.Should().NotBeNull();
+                    details.Players.Should().ContainSingle(p => p == "Player1");
+                }
+            }
+        }
+
+        [Fact]
+        public void PlayerOutOfTurn()
+        {
+            using (var context = DbTestHelper.GetNewContext())
+            {
+                var sut = new GameService(context, new ServiceLayer.Models.GameOptions());
+                NewGameDetails newGameDetails = new NewGameDetails()
+                {
+                    Columns = 4,
+                    Rows = 4,
+                    Players = new List<string>() { "Player1", "Player2" }
+                };
+                var createdGameId = sut.CreateNewGame(newGameDetails);
+                createdGameId.Should().NotBeNullOrEmpty();
+
+                // Valid play
+                {
+                    var move = sut.PlayMove(createdGameId, "Player1", new GameMove() { Column = 0, Type = GameMove.MoveType.MOVE });
+                    move.Should().NotBeNull();
+                    move.MoveId.Should().Be(1);
+                    move.Type.Should().Be(GameMove.MoveType.MOVE);
+                    move.Column.Should().Be(0);
+                }
+
+                // Invalid play -- trying to play again and skipping Player2
+                {
+                    Action act = () => sut.PlayMove(createdGameId, "Player1", new GameMove() { Column = 0, Type = GameMove.MoveType.MOVE });
+                    act.Should().Throw<PlayerTurnException>();
+                }
+
+                // Invalid play -- Player isn't in this game
+                {
+                    Action act = () => sut.PlayMove(createdGameId, "Invalid Player", new GameMove() { Column = 0, Type = GameMove.MoveType.MOVE });
+                    act.Should().Throw<PlayerTurnException>();
+                }
+
+                // Valid play
+                {
+                    var move = sut.PlayMove(createdGameId, "Player2", new GameMove() { Column = 0, Type = GameMove.MoveType.MOVE });
+                    move.Should().NotBeNull();
+                    move.MoveId.Should().Be(2);
+                    move.Type.Should().Be(GameMove.MoveType.MOVE);
+                    move.Column.Should().Be(0);
+                }
+            }
+        }
+
+        [Fact]
+        public void PlaySimpleGame()
+        {
+            using (var context = DbTestHelper.GetNewContext())
+            {
+                var sut = new GameService(context, new ServiceLayer.Models.GameOptions());
+                NewGameDetails newGameDetails = new NewGameDetails()
+                {
+                    Columns = 4,
+                    Rows = 4,
+                    Players = new List<string>() { "Player1", "Player2" }
+                };
+                var createdGameId = sut.CreateNewGame(newGameDetails);
+                createdGameId.Should().NotBeNullOrEmpty();
+
+                // Each player just plays straight into their own column and fills it except for the last piece.
+                for (int i = 0; i < (newGameDetails.Rows - 1) * newGameDetails.Players.Count; i++)
+                {
+                    var column = i % newGameDetails.Players.Count;
+                    var playerName = $"Player{column + 1}";
+                    var move = sut.PlayMove(createdGameId, playerName, new GameMove() { Column = column, Type = GameMove.MoveType.MOVE });
+                    move.Should().NotBeNull();
+                    move.MoveId.Should().Be(i + 1);
+                    move.Type.Should().Be(GameMove.MoveType.MOVE);
+                    move.Column.Should().Be(column);
+
+                    var details = sut.GetGameDetails(createdGameId);
+                    details.Should().NotBeNull();
+                    details.State.Should().Be(Game.GameState.IN_PROGRESS);
+                }
+
+                // Winning play - Player 1 wins!
+                {
+                    var move = sut.PlayMove(createdGameId, "Player1", new GameMove() { Column = 0, Type = GameMove.MoveType.MOVE });
+                    move.Should().NotBeNull();
+                    //move.MoveId.Should().Be(1);
+                    move.Type.Should().Be(GameMove.MoveType.MOVE);
+                    move.Column.Should().Be(0);
+
+                    var details = sut.GetGameDetails(createdGameId);
+                    details.Should().NotBeNull();
+                    details.State.Should().Be(Game.GameState.DONE);
+                    details.Winner.Should().Be("Player1");
+                }
             }
         }
     }
